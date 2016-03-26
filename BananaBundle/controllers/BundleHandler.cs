cd3caf08bf1080f305bbea2cc0aa12af76467a72
StormyCode <julian.heep@gmail.com>
2016-03-26 16:56:22 +0100

    added constructor and path property to BundleHandler

--- BananaBundle/controllers/BundleHandler.cs
+++ BananaBundle/controllers/BundleHandler.cs
@@ -3,12 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using BananaBundle.models;
 
 namespace BananaBundle.controllers
 {
     class BundleHandler
     {
-        public List<Serie> Series { get; set; }
+        public List<Serie> Series { get; private set; }
+
+        public string Path { get; set; }
+
+        public BundleHandler(string directory)
+        {
+            this.Series = new List<Serie>();
+            if (Directory.Exists(@directory))
+            {
+                this.Path = @directory;
+                foreach (string serieFolder in Directory.GetDirectories(this.Path))
+                {
+                    this.Series.Add(new Serie(serieFolder));
+                }
+            }
+        }
     }
 }
