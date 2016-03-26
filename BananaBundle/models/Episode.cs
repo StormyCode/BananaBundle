793a2b8f1e63060d0f68b182a04b80a4850853a0
StormyCode <julian.heep@gmail.com>
2016-03-26 16:24:29 +0100

    added Extension property to episode

--- BananaBundle/models/Episode.cs
+++ BananaBundle/models/Episode.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace BananaBundle.models
 {
-    class Episode
+    class Episode : IBananaObject
     {
+        public string Id
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Path
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public double Size
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Extension { get; set; }
     }
 }
