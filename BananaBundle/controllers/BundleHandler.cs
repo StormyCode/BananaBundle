40b29249da6306dfc14585b6c86ca4cbc884c4f3
StormyCode <julian.heep@gmail.com>
2016-03-26 16:53:19 +0100

    added list of series to BundleHandler

--- BananaBundle/controllers/BundleHandler.cs
+++ BananaBundle/controllers/BundleHandler.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BananaBundle.models;
 
 namespace BananaBundle.controllers
 {
     class BundleHandler
     {
+        public List<Serie> Series { get; set; }
     }
 }
