c9ae4c2e05e40796ad4367986a62e3a6fd89d26e
StormyCode <julian.heep@gmail.com>
2016-03-26 17:14:43 +0100

    added sys.dia to episode

--- BananaBundle/models/Episode.cs
+++ BananaBundle/models/Episode.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace BananaBundle.models
 {
+    [DebuggerDisplay("{Name}")]
     public class Episode : IBananaObject
     {
         
